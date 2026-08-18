54833ea5b665c7f91957c5e0879693b78fde0b20
Bhargav Giri <bhargavgiri10@gmail.com>
2026-08-18 19:02:32 +0530

    Time: 1 ms (72.17%), Space: 42.1 MB (94.62%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int total = n * (n + 1) / 2;
+
+        for (int x = 1; x <= n; x++) {
+            int left = x * (x + 1) / 2;
+            int right = total - (x - 1) * x / 2;
+
+            if (left == right) {
+                return x;
+            }
+        }
+
+        return -1;
+    }
+}
