c8c0dd771104c10f6101a2da22b2304becddc4bf
Bhargav Giri <bhargavgiri10@gmail.com>
2026-08-19 18:31:51 +0530

    Time: 17 ms (91.32%), Space: 53.8 MB (27.63%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        Map<Integer, Integer> rows = new HashMap<>();
+
+        for (int[] seat : reservedSeats) {
+            int row = seat[0];
+            int s = seat[1];
+
+            if (s >= 2 && s <= 9) {
+                rows.put(row, rows.getOrDefault(row, 0) | (1 << s));
+            }
+        }
+
+        int answer = (n - rows.size()) * 2;
+
+        for (int mask : rows.values()) {
+            boolean left = (mask & ((1 << 2) | (1 << 3) | (1 << 4) | (1 << 5))) == 0;
+            boolean middle = (mask & ((1 << 4) | (1 << 5) | (1 << 6) | (1 << 7))) == 0;
+            boolean right = (mask & ((1 << 6) | (1 << 7) | (1 << 8) | (1 << 9))) == 0;
+
+            if (left && right) {
+                answer += 2;
+            } else if (left || middle || right) {
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+}
